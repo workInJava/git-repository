4b81b7c8cff722307fdeb567b23ada05cd92a2d2
workInJava <wanghuaminhappy@163.com>
2015-04-06 13:23:27 +0800

    test

--- coreJava/src/com/corejava/test/Test1.java
+++ coreJava/src/com/corejava/test/Test1.java
@@ -1,5 +1,11 @@
 package com.corejava.test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Date;
+
+import com.corejava.day06.Book;
+
 public class Test1 extends TestF{
 	public void sayHello(){
 		System.out.println("Hello lijunli");
@@ -10,6 +16,16 @@ public class Test1 extends TestF{
 	}
 	public void play2() {
 		System.out.println("1");
+	}
+	
+	public static void main(String args[]) throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		long de = System.currentTimeMillis();
+		System.out.println(new Date(de));
+		
+		Book b = new Book();
+		Constructor<Book> f = (Constructor<Book>) b.getClass().getDeclaredConstructor();
+		Book b1 = f.newInstance(null);
+		System.out.println(b1==b);
 	}
 }
 
