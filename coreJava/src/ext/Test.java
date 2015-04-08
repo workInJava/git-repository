03b85d132ec0612d75cd70840bb2fdc5cb5ecd1f
workInJava <wanghuaminhappy@163.com>
2015-04-08 22:27:23 +0800

    master

--- coreJava/src/ext/Test.java
+++ coreJava/src/ext/Test.java
@@ -6,7 +6,7 @@ public class Test {
 	public static void main(String[] args) {
 		Test t = new Test();
 		int[] arr = {1,3,4,5,6};
-		System.out.println(t.get(arr, 2));
+	//	System.out.println(t.get(arr, 2));
 		
 		
 		TestInner n = new TestInner();
