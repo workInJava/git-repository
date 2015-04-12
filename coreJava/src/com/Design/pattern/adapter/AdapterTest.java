40a9ce1e4cce4037ad564770eb294fd316559624
workInJava <wanghuaminhappy@163.com>
2015-04-12 10:51:07 +0800

    adapter

--- coreJava/src/com/Design/pattern/adapter/AdapterTest.java
+++ coreJava/src/com/Design/pattern/adapter/AdapterTest.java
@@ -3,6 +3,7 @@ package com.Design.pattern.adapter;
 public class AdapterTest {
 
 	public static void main(String[] args){
+		//类适配模式，写一个Adapter类继承原类实现接口，那么这个类就可以满足这个接口了
 		Targetable t = new Adapter();
 		t.method1();
 		t.method2();
@@ -10,6 +11,17 @@ public class AdapterTest {
 		Targetable s = new Wrapper(source);
 		s.method1();
 		s.method2();
+		System.out.println("========================");
+		Sourceable s1 = new Source1();
+		Sourceable s2 = new Source2();
+		
+		//适配器模式借助抽象类实现接口所有方法，实现类不用直接与接口打交道，继承抽象类重写自己想要的方法
+		s1.method1();//覆盖抽像类的方法
+		s1.method2();
+		
+		s2.method1();
+		s2.method2();//覆盖抽像类的方法
+	
 	}
 	
 	//适配器模式
--- coreJava/src/com/Design/pattern/adapter/Wrapper2.java
+++ coreJava/src/com/Design/pattern/adapter/Wrapper2.java
@@ -3,9 +3,13 @@ package com.Design.pattern.adapter;
 public abstract class Wrapper2 implements Sourceable {
 
 	@Override
-	public void method1() {}
+	public void method1() {
+		System.out.println("Wrapper2 method1");
+	}
 
 	@Override
-	public void method2() {}
+	public void method2() {
+		System.out.println("Wrapper2 method2");
+	}
 
 }
